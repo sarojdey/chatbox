24f32c3e4ad8b23c53288bd680f346d72c9471d3
Saroj Kumar Dey <sarojkumardey.contact@gmail.com>
2025-05-14 14:16:17 +0530

    Refactor code structure for improved readability and maintainability

--- frontend/src/components/AuthImagePattern.jsx
+++ frontend/src/components/AuthImagePattern.jsx
@@ -2,7 +2,7 @@ const AuthImagePattern = ({ title, subtitle }) => {
   return (
     <div className="hidden lg:flex items-center justify-center bg-base-200 p-12">
       <div className="max-w-md text-center">
-        <div className="grid grid-cols-3 gap-3 mb-8">
+        {/* <div className="grid grid-cols-3 gap-3 mb-8">
           {[...Array(9)].map((_, i) => (
             <div
               key={i}
@@ -11,8 +11,9 @@ const AuthImagePattern = ({ title, subtitle }) => {
               }`}
             />
           ))}
-        </div>
-        <h2 className="text-2xl font-bold mb-4">{title}</h2>
+        </div> */}
+        <img src="/chat.png" alt="chat" className="w-full h-full" />
+        <h2 className="text-2xl font-bold mt-8 mb-2">{title}</h2>
         <p className="text-base-content/60">{subtitle}</p>
       </div>
     </div>
